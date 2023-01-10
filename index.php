97e2f649cb62cd44876cb12586ec0bcc9b03f25a
bryan1551998 <104786802+bryan1551998@users.noreply.github.com>
2023-01-10 11:40:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+    $mail->isSMTP(); //Send using SMTP
+    $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+    $mail->SMTPAuth = true; //Enable SMTP authentication
+    $mail->Username = 'balexis@balexis.com'; //SMTP username
+    $mail->Password = 'balexis!2022'; //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('balexis@balexis.com', 'Mailer');
+    $mail->addAddress('bryanpibaque1@gmail.com', 'Joe User'); //Add a recipient
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
+
+    //Content
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Tu correo ha sido ENVIADO!';
+} catch (Exception $e) {
+    echo "FATAL ERROR NO ENVIADO: {$mail->ErrorInfo}";
+
+}
+
+?>
